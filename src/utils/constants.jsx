eaf6eeef474e9981d954f52a54303664ff5f4458
Amritansh Raizada <110882945+AmritanshRaizada@users.noreply.github.com>
2025-07-21 01:34:14 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
 export const logo = "https://marketplace.canva.com/EAGKDhXEhoY/1/0/1600w/canva-brown-and-white-simple-modern-professional-catering-logo-Dvz9NG3gqk0.jpg";
 
 //named export
-export const menuURL="https://envieeats-proxy.onrender.com/api/menu?resid=" 
+export const menuURL="https://envieeats-proxy-mrc2.onrender.com/api/menu?resid=" 
